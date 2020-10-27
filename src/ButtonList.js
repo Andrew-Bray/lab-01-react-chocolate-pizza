7712d5572d029878fe7d93b96bf559101d4ff570
Andrew-Bray <amichaelbray@gmail.com>
2020-10-26 17:40:00 -0700

    alt added

--- src/ButtonList.js
+++ src/ButtonList.js
@@ -4,15 +4,15 @@ export default class ButtonList extends Component {
     render() {
         return (
             <div className="social-media">
-            <img src="assets/fb-icon.png" />
-            <img src="assets/twit-icon.png" />
-            <img src="assets/gp-icon.png" />
-            <img src="assets/insta-icon.png" />
-            <img src="assets/flic-icon.png" />
-            <img src="assets/pint-icon.png" />
-            <img className="contact-icon" src="assets/rss-icon.png" />
-            <img src="assets/mail-icon.png" />
-        </div>
+                <img src="assets/fb-icon.png" alt="icons" />
+                <img src="assets/twit-icon.png" alt="icons" />
+                <img src="assets/gp-icon.png" alt="icons" />
+                <img src="assets/insta-icon.png" alt="icons" />
+                <img src="assets/flic-icon.png" alt="icons" />
+                <img src="assets/pint-icon.png" alt="icons" />
+                <img className="contact-icon" src="assets/rss-icon.png" alt="icons" />
+                <img src="assets/mail-icon.png" alt="icons" />
+            </div>
         )
     }
 }
--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,9 @@ export default class Footer extends Component {
     render() {
         return (
             <footer>
-                <hr className="bottom-divider-r"/>
-                <img src="assets/small-logo.png"/>
-                <hr className="bottom-divider-l"/>
+                <hr className="bottom-divider-r" />
+                <img src="assets/small-logo.png" alt="logo" />
+                <hr className="bottom-divider-l" />
             </footer>
         )
     }
